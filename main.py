3ed1ea9e7cd7537c2ec1d038eca080ad3c986ab9
Damrukesh <ddamrukesh@gmail.com>
2026-01-14 13:04:45 +0530

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from database import engine
 import models
 from routes import router
+from dotenv import load_dotenv
+load_dotenv()
+
 
 models.Base.metadata.create_all(engine)
 
